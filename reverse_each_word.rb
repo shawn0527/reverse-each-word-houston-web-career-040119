a6d7b3efa6c9db85ab229c15097c676782a7ed9e
Shawn Ren <doggy-ren@hotmail.com>
2019-02-21 21:05:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split(" ")
-  result_array = sentence_array.collect {|x| x.reverse}
+  result_array = sentence_array.each {|x| x.reverse}
   result = result_array.join(" ")
 end
